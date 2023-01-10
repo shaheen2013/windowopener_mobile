01be11f88587fe5a6097e9e2f2dcdfd088f147fc
shaheen2013 <coredeveloper.2013@gmail.com>
2023-01-10 21:33:07 +0600

    Working on user page update

--- src/screens/Settings/index.js
+++ src/screens/Settings/index.js
@@ -1,41 +1,43 @@
-import React, {useState} from 'react';
-import {observer} from 'mobx-react';
-import {Column, FormControl, Input, Text} from 'native-base';
-import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
-import ActionButton from '@/components/buttons/ActionButton';
-import {object, string} from 'yup';
-import {errorMessage} from '@/utils/Yup';
-import FormInput from '@/components/FormInput';
-import {useStore} from '@/hooks';
-import * as Api from '@/services/api';
-import {apiError2Message} from '@/utils';
-import {assignIn} from 'lodash';
-import {useNavigation} from '@react-navigation/native';
+import ActionButton from "@/components/buttons/ActionButton";
+import FormInput from "@/components/FormInput";
+import { useStore } from "@/hooks";
+import * as Api from "@/services/api";
+import { apiError2Message } from "@/utils";
+import { useNavigation } from "@react-navigation/native";
+import { observer } from "mobx-react";
+import { Checkbox, Column, FormControl, Text } from "native-base";
+import React, { useState } from "react";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+// import RadioFilledIcon from "../../assets/svg/radio-filled.svg";
+// import RadioEmptyIcon from "../../assets/svg/radio-empty.svg";
 
 const Settings = () => {
-  const [name, setName] = useState('');
-  const [address, setAddress] = useState('');
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [isSelected, setIsSelected] = useState(false);
   const store = useStore();
   const nav = useNavigation();
 
+  
   const loadProfile = async () => {
     try {
       store.hud.show();
-      const {name, address} = await Api.getUserProfile();
+      const { name, address } = await Api.getUserProfile();
       setName(name);
       setAddress(address);
-    }catch(ex){
+    } catch (ex) {
       const apiError = apiError2Message(ex);
       if (apiError) {
         store.notification.showError(apiError);
       } else {
         store.notification.showError(ex.message);
       }
-    }
-    finally {
+    } finally {
       store.hud.hide();
     }
-  }
+  };
 
   React.useEffect(() => {
     loadProfile().then().catch(console.log);
@@ -44,18 +46,17 @@ const Settings = () => {
   const onPressSave = async () => {
     try {
       store.hud.show();
-      await Api.updateUserProfile({name, address})
-      store.notification.showSuccess("Profile updated");
+      await Api.updateUserProfile({ name, address });
+      store.notification.showSuccess('Profile updated');
       nav.goBack();
-    }catch(ex){
+    } catch (ex) {
       const apiError = apiError2Message(ex);
       if (apiError) {
         store.notification.showError(apiError);
       } else {
         store.notification.showError(ex.message);
       }
-    }
-    finally {
+    } finally {
       store.hud.hide();
     }
   };
@@ -72,21 +73,34 @@ const Settings = () => {
           <FormInput
             onChangeText={setName}
             value={name}
-            textContentType={'name'}
-            autoCapitalize={'words'}
+            textContentType={"name"}
+            autoCapitalize={"words"}
           />
         </FormControl>
         <FormControl mt={3}>
           <FormControl.Label>Address</FormControl.Label>
-          <FormInput
-            onChangeText={setAddress}
-            value={address}
-          />
+          <FormInput onChangeText={setAddress} value={address} />
         </FormControl>
-        <ActionButton mt={5} onPress={onPressSave}>Save</ActionButton>
+        <FormControl mt={3}>
+            <Checkbox checked={true} color="green"/>
+        </FormControl>
+        
+        <FormControl mt={3}>
+          <FormControl.Label>Latitude</FormControl.Label>
+          <FormInput onChangeText={setLatitude} value={latitude} />
+        </FormControl>
+        <FormControl mt={3}>
+          <FormControl.Label>Longitude</FormControl.Label>
+          <FormInput onChangeText={setLongitude} value={longitude} />
+        </FormControl>
+        <ActionButton mt={5} onPress={onPressSave}>
+          Save
+        </ActionButton>
       </KeyboardAwareScrollView>
     </Column>
-  )
+  );
 };
 
 export default observer(Settings);
+
+
